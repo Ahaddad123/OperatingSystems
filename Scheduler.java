d1e1048e8b862ad84e9c19a5c551eb799018d28c
Andrew H <68835521+Ahaddad123@users.noreply.github.com>
2022-06-16 17:17:00 -0500

    Delete Scheduler.java

--- Scheduler.java
+++
@@ -1,8 +0,0 @@
-public class Scheduler
-{
-   private static final long startTime = System.currentTimeMillis();
-   protected static final long age()
-   {
-      return System.currentTimeMillis() - startTime;
-   }
-}
